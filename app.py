5df6baab2d5b6a5497f7c2e876f4de7da25addc3
Максим Ландау <12landay@mail.ru>
2024-03-20 18:14:53 +0300

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/test')
+def test_function():
+    now = datetime.datetime.now().utcnow()
+    return f'Это тестовая страничка, ответ сгенерирован в {now}'
+
+
+
